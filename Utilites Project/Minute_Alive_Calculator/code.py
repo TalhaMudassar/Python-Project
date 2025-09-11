fe1a8d47ff0a00a2724f29c826e9963df3ff61bd
TalhaMudassar <talhamudassar200@gmail.com>
2025-09-11 16:18:42 +0500

    new code

--- Utilites Project/Minute_Alive_Calculator/code.py
+++ Utilites Project/Minute_Alive_Calculator/code.py
@@ -30,3 +30,36 @@ Bonus:
 - Let the user try again without restarting the program
 """
 
+
+def calculator_minute(age_years):
+    DAYS_IN_YEAR = 365.25
+    HOURS_IN_DAYS = 24
+    MINUTE_IN_HOUR = 60
+    SECOND_IN_MINUTE = 60
+    
+    total_days = DAYS_IN_YEAR * age_years
+    total_hours = total_days * HOURS_IN_DAYS
+    total_minutes = total_hours * MINUTE_IN_HOUR
+    total_seconds = total_minutes * SECOND_IN_MINUTE
+    
+    return round(total_days), round(total_hours), round(total_minutes),round(total_seconds)
+
+
+while True:
+    try:
+        age = float(input("Enter age: "))
+        days,hours,minutes,seconds = calculator_minute(age)
+        print("\nYou are approximately: ")
+        print(f"DAYS: {days:,}")
+        print(f"HOURS: {hours:,}")
+        print(f"MINUTES: {minutes:,}")
+        print(f"SECONDS: {seconds:,}")
+        
+        again = input("if you want to continue (y/n)")
+        if again != "y":
+            print("CODE COMPLETE ☺")
+            break
+    except:
+        print("Please enter a valid number of age ")
+        
+    
