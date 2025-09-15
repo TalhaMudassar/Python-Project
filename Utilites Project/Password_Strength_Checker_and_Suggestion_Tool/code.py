8c7a3edc8827eada1ffabb6f797171e5b159b401
TalhaMudassar <talhamudassar200@gmail.com>
2025-09-16 02:00:53 +0500

    new project add

--- Utilites Project/Password_Strength_Checker_and_Suggestion_Tool/code.py
+++ Utilites Project/Password_Strength_Checker_and_Suggestion_Tool/code.py
@@ -17,3 +17,43 @@ Your program should:
 Bonus:
 - Hide password input using `getpass` (no echo on screen).
 """
+
+import string
+import random
+import getpass
+
+def check_passward_strength(password):
+    issues = []
+    if len(password) < 8:
+        issues.append("Too short minimum 8 charcters allowed")
+    if not any(c.islower() for c in password):
+        issues.append("Missing Lower case letter")
+    if not any(c.isupper() for c in password):
+        issues.append("Missing Upper case letter")
+    if not any(c.isdigit() for c in password):
+        issues.append("Digit is missing")
+    if not any(c in string.punctuation for c in password):
+        issues.append("Missing Special Character")
+    return issues
+
+def generate_strong_password(length=12):
+    chars = string.ascii_letters + string.digits + string.punctuation
+    random.choice(chars)
+    return "".join(random.choice(chars) for _ in range(length))
+
+
+password = getpass.getpass("Enter a passward ")
+issues = check_passward_strength(password)
+if not issues:
+    print("Strong Password Good to go ")
+else:
+    print("You got weak password")
+    for issue in issues:
+        print(f"issues - {issue}")  
+        
+suggestion = generate_strong_password()
+print("\n Suggestion you strong password")
+print(suggestion)
+
+      
+        
